18324618e2480bfc7c8ef6288e163bff5ae5c25d
Shlok Parida <shlokparida@gmail.com>
2019-10-19 02:32:20 +0530

    ADDED BOOTSTRAP

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    blog_title = models.CharField(max_length = 350) 
+    blog_post = models.TextField() 
+    pub_date = models.DateTimeField() 
+    blog_image = models.ImageField(upload_to = 'media/images/blog')
--- jobs/models.py
+++ jobs/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Job(models.Model):
-    image = models.ImageField(upload_to = 'media/images/')
+    job_title = models.CharField(max_length = 255)
+    image = models.ImageField(upload_to = 'media/images/jobs')
     summary = models.CharField(max_length = 200)
-    link = models.CharField(max_length = 1000)
